8468eb3cef86dc63f84259467e923a38abc5a49c
evanknep <knepx001@umn.edu>
2024-03-17 20:05:10 -0500

    working inputs for gender/sex and shaded error bars

--- R/app.R
+++ R/app.R
@@ -15,7 +15,7 @@ ui <- fluidPage(
     # Application title
     titlePanel("PSY 8712 Week 8 Project"), #creating a project title that matches what I have in my markdown file
     selectInput('shaded_error', label = 'Display shaded error bars?', choices = c('Display Error Band', 'Suppress Error Band'), selected = "Display Error Band"), #creating a input select for displaying shaded error bars
-    selectInput('sex', label = "Select Sex", choices = c('Male', 'Female', 'All'), selected = 'All'), #creating input select for sex
+    selectInput('sex', label = "Select Sex", choices = c('Male' = 'M', 'Female' = 'F', 'All'), selected = 'All'), #creating input select for sex
     selectInput('date', label = "Exclude Participants from before July 1, 2017?", choices = c('Exclude', 'Do not exclude'), selected = "Do not exclude"), #creating input select for excluding pre 2017
     
     
@@ -26,13 +26,26 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  filtered_data <- reactive({
+    if (input$sex == 'All') {
+      data
+    } else {
+      data %>%
+        filter(gender == input$sex)
+    }
+  })
+      
   
-      output$corr_plot <- renderPlot({
-        data %>%
-          ggplot(aes(x=q1_q6_mean, y=q8_q10_mean)) +
-          geom_point() + 
-          geom_smooth(method = "lm", color = "purple") + 
-          labs(x = "q1-q6 mean", y = "q8-q10 mean", title = "Correlation of Q1-Q6 and Q8-Q10 Question Scores")
+  output$corr_plot <- renderPlot({
+    ggplot(filtered_data(), aes(x=q1_q6_mean, y=q8_q10_mean)) +
+      geom_point() + 
+      labs(x = "q1-q6 mean", y = "q8-q10 mean", title = "Correlation of Q1-Q6 and Q8-Q10 Question Scores") +
+      if (input$shaded_error == 'Display Error Band') {
+        geom_smooth(method = "lm", color = "purple")
+      }
+        
+        
+        
       })
 
    
